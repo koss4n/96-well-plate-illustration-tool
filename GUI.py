7f95f0d54672d4117f63125df0d154e8628cd0fb
koss4n <isaacarzoomand@gmail.com>
2024-06-03 14:50:03 +0200

    96-grid added with circles

--- GUI.py
+++ GUI.py
@@ -5,13 +5,13 @@ import numpy as np
 
 ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-
-
+letters_96size:list[str] = ["A","B","C","D","E","F","G","H"]
+grid_size_list:list[str] = ["96"]
 class App(ctk.CTk):
   def __init__(self):
       super().__init__()
       self.title("Randomizer App.py")
-      self.geometry(f"{1000}x{1000}")
+      self.geometry(f"{1600}x{900}")
       # configure grid layout (2x2)
       self.grid_columnconfigure(1, weight=1)
       self.grid_rowconfigure((0, 1), weight=1)
@@ -24,34 +24,42 @@ class App(ctk.CTk):
       self.logo_label = ctk.CTkLabel(self.sidebar_frame, text="Randomizer App", font=ctk.CTkFont(size=20, weight="bold"))
       self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
       
-      self.generate_well_grid = ctk.CTkButton(self.sidebar_frame, text="Gen Grid",
+      self.generate_well_grid = ctk.CTkOptionMenu(self.sidebar_frame, values=grid_size_list,
                                               command = self.create_well_grid_event)
       self.generate_well_grid.grid(row=1,column=0,padx=20,pady=20)
-      self.canvas = ctk.CTkCanvas(master=self, width = 650, height = 450, highlightcolor="blue")
+      self.canvas = ctk.CTkCanvas(master=self, width = 900, height = 800, highlightcolor="blue")
       self.canvas.grid(row=0,column=1, pady=(50))
-      self.canvas.create_aa_circle(x_pos=50,y_pos=50,radius=20,fill="blue")
-      self.canvas.create_aa_circle(x_pos=100,y_pos=50,radius=20,fill="blue")
-      self.canvas.create_aa_circle(x_pos=600,y_pos=50,radius=20,fill="blue")
-      self.canvas.create_aa_circle(x_pos=50,y_pos=400,radius=20,fill="blue")
       
-      self.canvas.scan
+        
+          
+          
+        
       
-      print(self.canvas.grid_bbox(column=1,row=2))
+      
+  
+    
       
       
       self.canvas_under_frame = ctk.CTkFrame(self, width=200,height=200)
       self.canvas_under_frame.grid(row=1,column=1)
      
     
-  def create_well_grid_event(self):
+  def create_well_grid_event(self, grid_size:str):
     
-    coordinates = ctk.CTkInputDialog(text="only int")
-    intg = coordinates.get_input().split(',')
-    x = int(intg[0])
-    y=int(intg[1]) 
-    ar = np.zeros(shape=(x,y))
+    if grid_size == "96":
+      self.create_canvas(space=70)
     
-    print(ar)
+  def create_canvas(self, space:int):
+    
+    i = space
+    for letter in letters_96size:
+      for j in range(12):
+        self.canvas.create_aa_circle(x_pos=space*(j+1),y_pos=i,radius=22,fill="black")
+        self.canvas.create_aa_circle(x_pos=space*(j+1),y_pos=i,radius=20,fill="white")
+      self.canvas.create_text(15,i,text=letter, fill="black", font=('Helvetica 15'))
+      i+=space
+    
+      
     
 if __name__ == "__main__":
     app = App()
