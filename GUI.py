ded1d5cce91a8f4a2aa36516c5d7d1f3096596f9
koss4n <isaacarzoomand@gmail.com>
2024-06-10 11:44:03 +0200

    Cleaned code, and added undo function

--- GUI.py
+++ GUI.py
@@ -24,6 +24,8 @@ class App(ctk.CTk):
       self.circle_color_map ={}
       self.canvas_items_map = {}
       self.canvas_id_text_map = {}
+      self.actions_stack = []
+      self.item_changes_map = {}
       self.title("Well Template.py")
       self.geometry(f"{1600}x{900}")
       # configure grid layout (2x2)
@@ -100,14 +102,16 @@ class App(ctk.CTk):
       self.canvas.bind("<Shift-B1-ButtonRelease>", self.add_text_selected_items)
       self.canvas.bind("<Shift-B1-Motion>", self.drag)
       self.canvas.bind("<Shift-Button-1>", self.callback)
+      self.canvas.bind("<Alt-B1-ButtonRelease>", self.add_rect_selected_area)
+      self.canvas.bind("<Alt-B1-Motion>", self.drag)
+      self.canvas.bind("<Alt-Button-1>", self.callback)
+      self.bind("<Control-z>", self.undo_action)
           
           
       
       
       self.canvas_under_frame = ctk.CTkFrame(self, width=900,height=200)
       self.canvas_under_frame.grid(row=1,column=2)
-      
-      
       
       
       
@@ -143,30 +147,26 @@ class App(ctk.CTk):
         id = self.canvas.create_aa_circle(x,y,radius=radius,fill="white")
         coords_circle = x,y
         self.canvas_items_map[id] = coords_circle
+        self.item_changes_map[id] = ["white"]
         
         
-        
-     
-    
-    
-  
   #Manipulates all circles overlapping with click-drag rectangle
   def release(self,event):
     global rectx1, recty1, rectx2, recty2
-    print("released at", event.x, event.y)
     
     self.canvas.coords(self.rect_id,0,0,0,0)
-    a= self.canvas.find_overlapping(rectx1,recty1,rectx2,recty2)
+    items_overlapping = self.canvas.find_overlapping(rectx1,recty1,rectx2,recty2)
     #Ignores items that aren't inner circle
-    item_list = []
-    for item in a:
+    list_of_items = []
+    for item in items_overlapping:
       if item in self.canvas_items_map:
-        item_list.append(item)
+        list_of_items.append(item)
         self.canvas.itemconfig(item,fill=self.color, tags=self.color)
+        self.item_changes_map[item].append(self.color)
         
         #self.canvas.create_text(self.canvas_items_map[item],text="Test", tags = "text-item")
-    self.circle_color_map[self.color] = item_list
-        
+    tuple = (list_of_items,"color_change")
+    self.actions_stack.append(tuple)   
   
     
     
@@ -177,7 +177,6 @@ class App(ctk.CTk):
     
   def callback(self,event):
     global rectx1, recty1,rectx2,recty2
-    print("clicked at", event.x,event.y)
     rectx1, rectx2 = event.x, event.x
     recty1,recty2= event.y,event.y
   
@@ -214,25 +213,33 @@ class App(ctk.CTk):
   
   def add_text_selected_items(self,event):
     global rectx1, recty1, rectx2, recty2
-    print("released at", event.x, event.y)
-    
     self.canvas.coords(self.rect_id,0,0,0,0)
     input_text = ctk.CTkInputDialog(text="Write Text",title="Add Text")
     text = input_text.get_input()
-    a= self.canvas.find_overlapping(rectx1,recty1,rectx2,recty2)
+    items_overlapping = self.canvas.find_overlapping(rectx1,recty1,rectx2,recty2)
     #Ignores items that aren't inner circle
-    
+    list_of_items = []
 #   #checks if item id is inner circle, and if it already exists as a text-item or not.
-    for item in a:
+    for item in items_overlapping:
+      
+      #If exists configure existing text-item
       if item in self.canvas_items_map and item in self.canvas_id_text_map: 
+        
         text_item = self.canvas_id_text_map[item] 
         self.canvas.itemconfigure(text_item, text=text)
-
+        self.item_changes_map[text_item].append(text)
+        list_of_items.append(text_item)
+      #If not exists create text-item and add to map
       elif item in self.canvas_items_map and item not in self.canvas_id_text_map:
         coords_ = self.canvas_items_map[item]
         text_item = self.canvas.create_text(coords_, text=text, tags="text", font = self.font)
         self.canvas_id_text_map[item] = text_item
+        self.item_changes_map[text_item] = [""]
+        self.item_changes_map[text_item].append(text)
+        list_of_items.append(text_item)
         
+    tuple = (list_of_items, "text_change")
+    self.actions_stack.append(tuple)    
   
   def change_font(self, var):
     var
@@ -251,15 +258,16 @@ class App(ctk.CTk):
         fill = app.winfo_rgb(fill)
         fill = fill[0]%256,fill[1]%256,fill[2]%256
         fill += (alpha,)
-        print(fill)
         image = Image.new('RGBA', (x2-x1, y2-y1), color=fill)
         images.append(ImageTk.PhotoImage(image))
-        self.canvas.create_image(x1, y1, image=images[-1], anchor='nw')
-    self.canvas.create_rectangle(x1, y1, x2, y2, **kwargs)
-
+        b = self.canvas.create_image(x1, y1, image=images[-1], anchor='nw')
+    rect = self.canvas.create_rectangle(x1, y1, x2, y2, **kwargs)
+    ids = [b,rect]
+    tuple = (ids,"item_created")
+    self.actions_stack.append(tuple) 
+    
   def add_rect_selected_area(self,event):
     global rectx1, recty1, rectx2, recty2
-    print("released at", event.x, event.y)
     
     self.canvas.coords(self.rect_id,0,0,0,0)
     a= self.canvas.find_overlapping(rectx1,recty1,rectx2,recty2)
@@ -277,16 +285,41 @@ class App(ctk.CTk):
     
     coords1 = self.canvas_items_map[b]
     coords2 = self.canvas_items_map[c]
-    print(coords1)
+    
     l = self.radius+4
-    print(l)
+    
     x1, y1 = coords1
     x2,y2 = coords2
-    print(x1)
+    
     self.create_rectangle(x1-l, y1-l, x2+l, y2+l, fill=self.color, alpha=.2)
-  
     
+
     
+  def undo_action(self, event):
+    
+    if len(self.actions_stack) < 1:
+      return
+    
+    prev_action = self.actions_stack.pop()
+    items = prev_action[0]
+    
+    if prev_action[1] =="color_change":
+      
+      for item in items:
+        current_color = self.item_changes_map[item].pop()
+        prev_color = self.item_changes_map[item][len(self.item_changes_map[item])-1]
+        self.canvas.itemconfigure(item, fill = prev_color)
+        
+    if prev_action[1] =="text_change":
+      for item in items:
+        current_text = self.item_changes_map[item].pop()
+        prev_text = self.item_changes_map[item][len(self.item_changes_map[item])-1]
+        self.canvas.itemconfigure(item, text = prev_text)   
+    
+    if prev_action[1] == "item_created":
+      for item in items:
+        self.canvas.delete(item)
+      
     
 if __name__ == "__main__":
     app = App()
